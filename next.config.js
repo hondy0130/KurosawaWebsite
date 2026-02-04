f25229a3737a9a83b5d6af7d1ac7130cb28eb563
Kurosawa <honda@kurosawa.vn>
2026-02-04 12:37:08 +0700

    chore: update components and config files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-module.exports = nextConfig;
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.microcms-assets.io',
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
