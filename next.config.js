013ae236df4c7de39edad510aa82536032f6db86
Kurosawa <honda@kurosawa.vn>
2026-02-19 13:40:52 +0700

    add old site URL redirects to next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,76 @@ const nextConfig = {
         },
       ],
     },
+    async redirects() {
+      return [
+        // 旧サイト（kurosawa-vn.com）のURLパスを新サイトにリダイレクト
+        {
+          source: '/company.html',
+          destination: '/about',
+          permanent: true,
+        },
+        {
+          source: '/member.html',
+          destination: '/about#members',
+          permanent: true,
+        },
+        {
+          source: '/daihyou.html',
+          destination: '/',
+          permanent: true,
+        },
+        {
+          source: '/contact.html',
+          destination: '/#contact',
+          permanent: true,
+        },
+        {
+          source: '/form.html',
+          destination: '/#contact',
+          permanent: true,
+        },
+        {
+          source: '/faq.html',
+          destination: '/#faq',
+          permanent: true,
+        },
+        {
+          source: '/service.html',
+          destination: '/services',
+          permanent: true,
+        },
+        {
+          source: '/kyujin.html',
+          destination: '/careers',
+          permanent: true,
+        },
+        {
+          source: '/recruit.html',
+          destination: '/careers',
+          permanent: true,
+        },
+        {
+          source: '/blog.html',
+          destination: '/articles',
+          permanent: true,
+        },
+        {
+          source: '/blog:num(\\d+).html',
+          destination: '/articles',
+          permanent: true,
+        },
+        {
+          source: '/news.html',
+          destination: '/articles',
+          permanent: true,
+        },
+        {
+          source: '/news:num(\\d+).html',
+          destination: '/articles',
+          permanent: true,
+        },
+      ];
+    },
   };
   
   module.exports = nextConfig;
